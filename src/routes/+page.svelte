474563716d1fc5a437c1dacaafc794879b1ff34a
Pat McGhen <patrickmcghen@gmail.com>
2023-11-22 15:44:33 -0500

    Update url paths.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,3 @@
-<script>
-  import { base } from '$app/paths';
-</script>
-
 <svelte:head>
   <title>Pat McGhen | Full stack developer</title>
 </svelte:head>
@@ -14,6 +10,6 @@
 </p>
 
 <ul>
-  <li><a href="{base}/work/accessible-web-components">Accessible Web Components</a></li>
-  <li><a href="{base}/work/firefox-dev-tools">Firefox DevTools</a></li>
+  <li><a href="/work/accessible-web-components">Accessible Web Components</a></li>
+  <li><a href="/work/firefox-dev-tools">Firefox DevTools</a></li>
 </ul>
